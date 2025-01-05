f698070b67f4a89746251e5f1fe2747666374ce7
Maria de Fátima <mfatima5bc@gmail.com>
2025-01-05 11:19:07 -0300

    test: finish test to search for submissions

--- src/infra/http/graphql/resolvers/submission.resolver.e2e-spec.ts
+++ src/infra/http/graphql/resolvers/submission.resolver.e2e-spec.ts
@@ -4,6 +4,7 @@ import { DatabaseModule } from '@/infra/database/database.module';
 import { PrismaService } from '@/infra/database/prisma/prisma.service';
 import { INestApplication } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
+import { parseISO } from 'date-fns';
 import request from 'supertest';
 import { ChallengeFactory } from 'test/factories/make-challenge';
 import { SubmissionFactory } from 'test/factories/make-submission';
@@ -32,11 +33,11 @@ describe('Submissions list (E2E)', () => {
     const challenge = await challengeFactory.makePrismaChallenge({ title: 'Socket chat app', description: 'nodejs' } );
     challengeId = challenge.id.toString();
 
-    await submissionFactory.makePrismaSubmission({}, challenge.id);
-    await submissionFactory.makePrismaSubmission({}, challenge.id);
-    await submissionFactory.makePrismaSubmission({}, challenge.id);
-    await submissionFactory.makePrismaSubmission({}, challenge.id);
-    await submissionFactory.makePrismaSubmission({}, challenge.id);
+    await submissionFactory.makePrismaSubmission({createdAt: parseISO("2025-01-02"), status: 'Done'}, challenge.id);
+    await submissionFactory.makePrismaSubmission({createdAt: parseISO("2025-01-03"), status: 'Done'}, challenge.id);
+    await submissionFactory.makePrismaSubmission({createdAt: parseISO("2024-11-02")}, challenge.id);
+    await submissionFactory.makePrismaSubmission({createdAt: parseISO("2024-11-02")}, challenge.id);
+    await submissionFactory.makePrismaSubmission({createdAt: parseISO("2024-11-02")}, challenge.id);
   });
 
   it('Query submissions paginated', async () => {
@@ -52,8 +53,8 @@ describe('Submissions list (E2E)', () => {
               page: 1, 
               limit: 2
               status: Pending
-              startDate: "2025-01-01"
-              endDate: "2025-01-06"
+              startDate: "2024-11-02"
+              endDate: "2025-01-02"
             }
           ) {
             items {
@@ -70,5 +71,34 @@ describe('Submissions list (E2E)', () => {
     expect(items.length).toBe(2);
   });
 
+  it('Query submissions paginated and filter by date and status', async () => {
 
+    const response = await request(app.getHttpServer())
+      .post('/graphql')
+      .send({
+        query: `
+        query {
+          fetchSubmissions(
+            params: {
+              challengeId: "${challengeId}"
+              page: 1, 
+              limit: 2
+              status: Done
+              startDate: "2025-01-02"
+              endDate: "2025-01-03"
+            }
+          ) {
+            items {
+              status
+            }
+            hasMorePages
+          }
+        }
+      `,
+      });
+    const { data: { fetchSubmissions: { hasMorePages, items } } } = response.body;
+
+    expect(hasMorePages).toBe(false);
+    expect(items.length).toBe(2);
+  });
 });
